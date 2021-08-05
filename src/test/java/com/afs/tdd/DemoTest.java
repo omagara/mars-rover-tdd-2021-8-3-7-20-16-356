5c26c860b29c7804c44799a00a493bf67fae2adb
omagara <omagara@oocl.com>
2021-08-05 20:23:59 +0800

    [MarsRover][Ramon]test: write test for should_return_locationX_0_locationY_0_direction_W_when_execute_command_given_locationX_0_locationY_0_direction_N_and_L

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -23,4 +23,19 @@ class DemoTest {
         assertThat(marsRover.getRoverStatus().getLocationY()).isEqualTo(expectedRoverStatus.getLocationY());
         assertThat(marsRover.getRoverStatus().getDirection()).isEqualTo(expectedRoverStatus.getDirection());
     }
+
+    @Test
+    void should_return_locationX_0_locationY_0_direction_W_when_execute_command_given_locationX_0_locationY_0_direction_N_and_L(){
+        //given
+        MarsRover marsRover = new MarsRover(new RoverStatus(0,0,"N"));
+        RoverStatus expectedRoverStatus = new RoverStatus(0,0,"W");
+
+        //when
+        marsRover.executeCommand("L");
+
+        //then
+        assertThat(marsRover.getRoverStatus().getLocationX()).isEqualTo(expectedRoverStatus.getLocationX());
+        assertThat(marsRover.getRoverStatus().getLocationY()).isEqualTo(expectedRoverStatus.getLocationY());
+        assertThat(marsRover.getRoverStatus().getDirection()).isEqualTo(expectedRoverStatus.getDirection());
+    }
 }
